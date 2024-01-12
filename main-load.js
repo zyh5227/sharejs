df23bdb07cd12bf19a0497862c5d0f9dc5c44e97
yh <792249139@qq.com>
2024-01-12 14:18:14 +0800

    提交svg

--- main-load.js
+++ main-load.js
@@ -1,68 +1,71 @@
 function addFloatAdv(both, position, href, imageSrc) {
-    const div = document.createElement("div");
-    div.className = both + "-" + position + "-advertising advertising";
-    const closeAdv = document.createElement("div");
-    const htmlSpanElement = document.createElement("span");
-    htmlSpanElement.innerHTML = "关闭"
-    closeAdv.appendChild(htmlSpanElement)
-    const rect = document.createElement("span")
-    rect.className = "right-title-btn"
-    closeAdv.appendChild(rect)
-    closeAdv.className = "close-adv"
-    closeAdv.addEventListener('click', function () {
-        div.style.display = 'none'
-        window.open(href)
-    })
-    const a = document.createElement("a");
-    a.className = "advertising-a"
-    a.href = href
-    a.target = "_blank"
-    const image = document.createElement("img")
-    image.alt = "广告"
-    image.src = imageSrc
-    a.appendChild(image)
-    div.appendChild(closeAdv)
-    div.appendChild(a)
-    document.body.appendChild(div)
+  const div = document.createElement("div");
+  div.className = both + "-" + position + "-advertising advertising";
+  const closeAdv = document.createElement("div");
+  const htmlSpanElement = document.createElement("span");
+  htmlSpanElement.innerHTML = "关闭";
+  closeAdv.appendChild(htmlSpanElement);
+  const rect = document.createElement("span");
+  rect.className = "right-title-btn";
+  closeAdv.appendChild(rect);
+  closeAdv.className = "close-adv";
+  closeAdv.addEventListener("click", function () {
+    div.style.display = "none";
+    window.open(href);
+  });
+  const a = document.createElement("a");
+  a.className = "advertising-a";
+  a.href = href;
+  a.target = "_blank";
+  const image = document.createElement("img");
+  image.alt = "广告";
+  image.src = imageSrc;
+  a.appendChild(image);
+  div.appendChild(closeAdv);
+  div.appendChild(a);
+  document.body.appendChild(div);
 }
 
 function addTopOrBottomAdv(position, href, imageSrc) {
-    const div = document.createElement("div");
-    div.className = position + "-advertising advertising";
-    const a = document.createElement("a");
-    a.className = "advertising-a"
-    a.href = href
-    a.target = "_blank"
-    const image = document.createElement("img")
-    image.alt = "广告"
-    image.src = imageSrc
-    a.appendChild(image)
-    div.appendChild(a)
-    document.body.appendChild(div)
+  const div = document.createElement("div");
+  div.className = position + "-advertising advertising";
+  const a = document.createElement("a");
+  a.className = "advertising-a";
+  a.href = href;
+  a.target = "_blank";
+  const image = document.createElement("img");
+  image.alt = "广告";
+  image.src = imageSrc;
+  a.appendChild(image);
+  div.appendChild(a);
+  document.body.appendChild(div);
 }
 
 function addDelayMask(href, delayTime) {
-    const div = document.createElement("div");
-    div.className = "mask";
-    div.addEventListener('click', function () {
-        div.style.display = 'none'
-        window.open(href)
-    })
-    setTimeout(function () {
-        document.body.appendChild(div)
-    }, delayTime)
+  const div = document.createElement("div");
+  div.className = "mask";
+  div.addEventListener("click", function () {
+    div.style.display = "none";
+    window.open(href);
+  });
+  setTimeout(function () {
+    document.body.appendChild(div);
+  }, delayTime);
 }
 
 function loadStyle() {
-    const new_element = document.createElement('link');
-    new_element.setAttribute('rel', 'stylesheet');
-    new_element.setAttribute("type", "text/css");
-    // new_element.setAttribute('href', 'main-load.css');
-    new_element.setAttribute('href', 'https://analysis.zhantaotie.com/main-load.css');
-    document.head.appendChild(new_element);
+  const new_element = document.createElement("link");
+  new_element.setAttribute("rel", "stylesheet");
+  new_element.setAttribute("type", "text/css");
+  // new_element.setAttribute('href', 'main-load.css');
+  new_element.setAttribute(
+    "href",
+    "https://analysis.zhantaotie.com/main-load.css"
+  );
+  document.head.appendChild(new_element);
 }
 
-loadStyle()
+loadStyle();
 // addDelayMask("www.baidu.com", 5000)
 // addFloatAdv("right", "bottom", "https://g32554517s.zicp.fun/adv/jump?id=", 'https://fakeimg.pl/220x300/F44336/FFF/?font=noto&text=顶部广告')
 // addFloatAdv("left", "bottom", "https://g32554517s.zicp.fun/adv/jump?id=", 'https://fakeimg.pl/220x300/F44336/FFF/?font=noto&text=顶部广告')
@@ -85,4 +88,3 @@ loadStyle()
 //         addTopOrBottomAdv("bottom", "https://g32554517s.zicp.fun/adv/jump?id=" + json.topAdvId, "https://fakeimg.pl/1920x50/F44336/FFF/?font=noto&text=顶部广告")
 //     }
 // };
-
